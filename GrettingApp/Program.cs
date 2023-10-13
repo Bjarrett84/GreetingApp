209722f851faf944b5472bffcf14006dd40399cf
Brandon <48112914+Bjarrett84@users.noreply.github.com>
2023-10-12 21:29:50 -0700

    Add files via upload

---
+++ GrettingApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+//Welcome user To App
+Console.WriteLine("Welcome to the Greeting Application");
+Console.WriteLine("This application was built by Brandon Jarrett");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine();
+
+
+// Ask for First Name
+Console.Write(" What is your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+// Ask for Last Name
+
+Console.Write(" What is your last name: ");
+string lastName;
+lastName = Console.ReadLine(); 
+
+
+
+
+
+// Greet user by name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName + " " + lastName + "hope your doing well!");
+
+
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
+
+
+
+
